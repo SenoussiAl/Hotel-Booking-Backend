c45cd2d99c5fc523a8bfce7173382da7b459b5b9
SenoussiAl <84295873+SenoussiAl@users.noreply.github.com>
2025-10-18 14:03:44 -0400

    Completed Models, added Dtos and Mapper

--- HotelBookingSystem/obj/Debug/net9.0/HotelBookingSystem.AssemblyInfo.cs
+++ HotelBookingSystem/obj/Debug/net9.0/HotelBookingSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+36dab135ad87cbba40a264e4911b201dfc25b822")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4eeccdc7a150f564f2f1a467ea0a5fd3513749a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HotelBookingSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
